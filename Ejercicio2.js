3c98e9989fabffde4b89f883b7b1134d92436537
Ruben-Serrano2112 <75427020+Ruben-Serrano2112@users.noreply.github.com>
2021-05-10 11:50:42 +0200

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Ejemplo consola</title>
+    </head>
+    <body>
+
+		<script src="../java/Ejercicio2.js"></script>
+    </body>
+</html>
