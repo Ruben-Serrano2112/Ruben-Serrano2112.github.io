68577140753de4ed5aaf695538a71b0065204ac2
Ruben-Serrano2112 <75427020+Ruben-Serrano2112@users.noreply.github.com>
2021-05-10 12:20:27 +0200

    Update Ejercicio2.js

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,3 +1,12 @@
 'use strict';
 
+function gestionarFicheroXML(xmlDoc)
+{
+  let capaVacia = document.querySelector("#ficheroXML")
+  let libros = xmlDoc.querySelectorAll("libro")
+  for (let i = 0; i < alumnos.length; i++)
+    capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
+}
+
+
 
