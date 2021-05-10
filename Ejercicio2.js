acb668fe3e51f3de64f1aeef01e64052ab608a79
Ruben-Serrano2112 <75427020+Ruben-Serrano2112@users.noreply.github.com>
2021-05-10 11:54:06 +0200

    Update Ejercicio2.js

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,11 +1,3 @@
-<!DOCTYPE>
-<html>
-    <head>
-        <meta charset="UTF-8"/>
-        <title>Ejemplo consola</title>
-    </head>
-    <body>
+'use strict';
 
-		<script src="../java/Ejercicio2.js"></script>
-    </body>
-</html>
+
