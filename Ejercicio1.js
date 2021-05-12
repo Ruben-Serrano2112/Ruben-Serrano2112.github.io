780a086f1748079c96653466a5a22dd5e6690be6
Ruben-Serrano2112 <75427020+Ruben-Serrano2112@users.noreply.github.com>
2021-05-12 09:52:35 +0200

    Update Ejercicio1.js

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -5,7 +5,16 @@ function gestionarFicheroXML(xmlDoc)
   let capaVacia = document.querySelector("#ficheroXML")
   let libros = xmlDoc.querySelectorAll("libro")
   for (let i = 0; i < libros.length; i++)
-    capaVacia.innerHTML += "<div class='fila'>" + libros[i].textContent + "</div>"
+    capaVacia.innerHTML += "<div class='fila'>" + libros[i].textContent + "</div>" +
+    "<div class='columna'>"+libros[i].querySelector("ISBN").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("titulo").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("nivelProfundidad").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("autores").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("titulo").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("editorial").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("fechaPublicacion").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("paginaWeb").textContent +"<div>" +
+    "<div class='columna'>"+libros[i].querySelector("precio").textContent +"<div>"
     
 }
 
