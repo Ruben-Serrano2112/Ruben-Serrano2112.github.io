23371aa27d3538c64903c5071f7d4c03f4c543ca
Ruben-Serrano2112 <75427020+Ruben-Serrano2112@users.noreply.github.com>
2021-05-20 12:36:22 +0200

    Add files via upload

--- Ejercicio2/Ejercicio2.js
+++ Ejercicio2/Ejercicio2.js
@@ -1,20 +1,17 @@
-function gestionarFicheroTXT(txtDoc){ //Al ser un txt tienes que poner gestionarFicheroTXT(txtDoc)
+function gestionarFicheroTXT(txtDoc) {
 	alert(txtDoc)
 	let capaVacia = document.querySelector("#ficheroXML")
-	let conversacion = txtDoc.textContent("");  //Ya no puede hacer un querySelector del txtDoc. 
-	for(let i=0; i<conversacion.length; i++)
-		if (i % 2 == 0)
-		{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<div class='izquierda'>" + conversacion[i].textContent + "</div>"
+	let conversacion = txtDoc.Split("\n");
+	for (let i = 0; i < conversacion.length; i++)
+		if (i % 2 == 0) {
+			capaVacia.innerHTML = capaVacia.innerHTML + "<div class='izquierda'>" + conversacion[i].textContent + "</div>"
 		}
-		else
-		{
-		capaVacia.innerHTML = capaVacia.innerHTML + "<div class='derecha'>" + conversacion[i].textContent + "</div>"
+		else {
+			capaVacia.innerHTML = capaVacia.innerHTML + "<div class='derecha'>" + conversacion[i].textContent + "</div>"
 		}
 }
-let capa = document.querySelector("div") 
-capa.addEventListener("click",CargarFichero);
-function CargarFichero()
-{
-        loadDocA("Mensajes.txt","txt");
+let capa = document.querySelector("div")
+capa.addEventListener("click", CargarFichero);
+function CargarFichero() {
+	loadDocA("Mensajes.txt", "txt");
 }
